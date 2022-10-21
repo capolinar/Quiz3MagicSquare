b61569cc5ecdb093a15b75f6c74b2be58b0db049
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-21 15:49:00 -0700

    First commit comments/outline

---
+++ magicSquare.c
@@ -0,0 +1,28 @@
+//headers go here
+
+//magic square verification code to be called
+//check and verify if numbers are 1 to 9 (no 0 or 10+)
+//print if errors are found for numbers
+//check if each row, column, and diagnol add up to the same number
+//print if not magic square/reason
+//return true/false status
+
+
+//main goes here
+
+
+//part1
+//create 2d array for 3x3 square that is already polulated
+//and is also already a magic square
+//Test/verify if magic square
+
+//part2
+//create 2d array
+//populate square, no repeats
+//populaton is by random
+//check if magic square
+//keep count of loop
+//loop/randomize until it is a magic square
+//print square and placement
+
+//return success message
