530a5fbbf001c861ac9d63cd492a04a6b82623c7
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-22 11:55:07 -0500

    Added headers, main, and 2d array of magicSquare elements

--- magicSquare.c
+++ magicSquare.c
@@ -1,4 +1,9 @@
 //headers go here
+#include <stdlib.h> 
+#include  <time.h> 
+#include  <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 //magic square verification code to be called
 //check and verify if numbers are 1 to 9 (no 0 or 10+)
@@ -6,7 +11,28 @@
 //check if each row, column, and diagnol add up to the same number
 //print if not magic square/reason
 //return true/false status
+int main(void)
+{
+    //part1
+//create 2d array for 3x3 square that is already polulated
+//and is also already a magic square
+//Test/verify if magic square
+    int magicSq[3][3] = {4,9,2,3,5,7,8,1,6};//3x3 magic square
 
+    //add verify function here
+
+    printf("Pre-made qquare print out: \n")
+    {
+        
+    }
+    
+
+
+
+
+
+    return EXIT_SUCCESS
+}
 
 //main goes here
 
@@ -17,7 +43,7 @@
 //Test/verify if magic square
 
 //part2
-//create 2d array
+//create 2d arraygit
 //populate square, no repeats
 //populaton is by random
 //check if magic square
