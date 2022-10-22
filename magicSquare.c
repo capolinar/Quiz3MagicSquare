55c6efc1fffae4f9cc64a9047cf40c20aee3d4f1
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-22 12:04:52 -0500

    Fixed printSquare

--- magicSquare.c
+++ magicSquare.c
@@ -44,9 +44,9 @@ int main(void)
 
 void printSquare(int a[3][3])
 {
-    for(int i = 3; i < 3; i++)
+    for(int i = 0; i < 3; i++)
     {
-        for(int j = 3; j < 3; j++)
+        for(int j = 0; j < 3; j++)
         {
             printf("%d",a[i][j]);
         }
